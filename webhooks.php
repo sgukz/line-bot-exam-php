5f27c619135774409e6cfd2717a95af6f328b397
sgukz <thorapee055@gmail.com>
2018-08-20 08:33:07 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'KZFW6ZW36NTo2nWriM/bqpf4B2YmOsKkx0Ijd1Zl6n3oOBo//A2TErMT5oBAv5nKV0/xTLdDVC4vXDzfaufdeXStbBPgDNe4Of9WE/49lPkh00j0LDSEQZmauPnWQL4QAQOkKLIpYUmAfgV0BQSJYgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
